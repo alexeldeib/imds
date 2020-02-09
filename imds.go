10cc10618e161adb766b9cbd6ca882a2defcb7cc
Ace Eldeib <alexeldeib@gmail.com>
2020-02-09 03:08:27 -0800

    bump: version to 2019-08-15

--- imds.go
+++ imds.go
@@ -21,7 +21,7 @@ func New() (*Metadata, error) {
 
 	q := req.URL.Query()
 	q.Add("format", "json")
-	q.Add("api-version", "2019-03-11")
+	q.Add("api-version", "2019-08-15")
 	req.URL.RawQuery = q.Encode()
 
 	resp, err := client.Do(req)
@@ -57,51 +57,114 @@ type Metadata struct {
 	ParsedTags map[string]string `json:"parsedTags"`
 }
 
-type Compute struct {
-	AzEnvironment        string      `json:"azEnvironment"`
-	CustomData           string      `json:"customData"`
-	Location             string      `json:"location"`
-	Name                 string      `json:"name"`
-	Offer                string      `json:"offer"`
-	OsType               string      `json:"osType"`
-	PlacementGroupID     string      `json:"placementGroupId"`
-	Plan                 Plan        `json:"plan"`
-	PlatformFaultDomain  string      `json:"platformFaultDomain"`
-	PlatformUpdateDomain string      `json:"platformUpdateDomain"`
-	Provider             string      `json:"provider"`
-	PublicKeys           []PublicKey `json:"publicKeys"`
-	Publisher            string      `json:"publisher"`
-	ResourceGroupName    string      `json:"resourceGroupName"`
-	ResourceID           string      `json:"resourceId"`
-	Sku                  string      `json:"sku"`
-	SubscriptionID       string      `json:"subscriptionId"`
-	Tags                 string      `json:"tags"`
-	Version              string      `json:"version"`
-	VMID                 string      `json:"vmId"`
-	VMScaleSetName       string      `json:"vmScaleSetName"`
-	VMSize               string      `json:"vmSize"`
-	Zone                 string      `json:"zone"`
-}
-
 type Plan struct {
 	Name      string `json:"name"`
 	Product   string `json:"product"`
 	Publisher string `json:"publisher"`
 }
 
-type PublicKey struct {
+type PublicKeys struct {
 	KeyData string `json:"keyData"`
 	Path    string `json:"path"`
 }
 
-type Network struct {
-	Interface []Interface `json:"interface"`
+type Image struct {
+	URI string `json:"uri"`
 }
 
-type Interface struct {
-	IPv4       IPv4   `json:"ipv4"`
-	IPv6       IPv6   `json:"ipv6"`
-	MacAddress string `json:"macAddress"`
+type ManagedDisk struct {
+	ID                 string `json:"id"`
+	StorageAccountType string `json:"storageAccountType"`
+}
+
+type VHD struct {
+	URI string `json:"uri"`
+}
+
+type DataDisks struct {
+	Caching                 string      `json:"caching"`
+	CreateOption            string      `json:"createOption"`
+	DiskSizeGB              string      `json:"diskSizeGB"`
+	Image                   Image       `json:"image"`
+	LUN                     string      `json:"lun"`
+	ManagedDisk             ManagedDisk `json:"managedDisk"`
+	Name                    string      `json:"name"`
+	VHD                     VHD         `json:"vhd"`
+	WriteAcceleratorEnabled string      `json:"writeAcceleratorEnabled"`
+}
+
+type ImageReference struct {
+	ID        string `json:"id"`
+	Offer     string `json:"offer"`
+	Publisher string `json:"publisher"`
+	Sku       string `json:"sku"`
+	Version   string `json:"version"`
+}
+
+type DiffDiskSettings struct {
+	Option string `json:"option"`
+}
+
+type EncryptionSettings struct {
+	Enabled string `json:"enabled"`
+}
+
+type OsDisk struct {
+	Caching                 string             `json:"caching"`
+	CreateOption            string             `json:"createOption"`
+	DiffDiskSettings        DiffDiskSettings   `json:"diffDiskSettings"`
+	DiskSizeGB              string             `json:"diskSizeGB"`
+	EncryptionSettings      EncryptionSettings `json:"encryptionSettings"`
+	Image                   Image              `json:"image"`
+	ManagedDisk             ManagedDisk        `json:"managedDisk"`
+	Name                    string             `json:"name"`
+	OsType                  string             `json:"osType"`
+	VHD                     VHD                `json:"vhd"`
+	WriteAcceleratorEnabled string             `json:"writeAcceleratorEnabled"`
+}
+
+type StorageProfile struct {
+	DataDisks      []DataDisks    `json:"dataDisks"`
+	ImageReference ImageReference `json:"imageReference"`
+	OsDisk         OsDisk         `json:"osDisk"`
+}
+
+type Compute struct {
+	AzEnvironment        string         `json:"azEnvironment"`
+	CustomData           string         `json:"customData"`
+	Location             string         `json:"location"`
+	Name                 string         `json:"name"`
+	Offer                string         `json:"offer"`
+	OsType               string         `json:"osType"`
+	PlacementGroupID     string         `json:"placementGroupId"`
+	Plan                 Plan           `json:"plan"`
+	PlatformFaultDomain  string         `json:"platformFaultDomain"`
+	PlatformUpdateDomain string         `json:"platformUpdateDomain"`
+	Provider             string         `json:"provider"`
+	PublicKeys           []PublicKeys   `json:"publicKeys"`
+	Publisher            string         `json:"publisher"`
+	ResourceGroupName    string         `json:"resourceGroupName"`
+	ResourceID           string         `json:"resourceId"`
+	Sku                  string         `json:"sku"`
+	StorageProfile       StorageProfile `json:"storageProfile"`
+	SubscriptionID       string         `json:"subscriptionId"`
+	Tags                 string         `json:"tags"`
+	TagsList             []interface{}  `json:"tagsList"`
+	Version              string         `json:"version"`
+	VMID                 string         `json:"vmId"`
+	VMScaleSetName       string         `json:"vmScaleSetName"`
+	VMSize               string         `json:"vmSize"`
+	Zone                 string         `json:"zone"`
+}
+
+type IPAddress struct {
+	PrivateIPAddress string `json:"privateIpAddress"`
+	PublicIPAddress  string `json:"publicIpAddress"`
+}
+
+type Subnet struct {
+	Address string `json:"address"`
+	Prefix  string `json:"prefix"`
 }
 
 type IPv4 struct {
@@ -110,15 +173,15 @@ type IPv4 struct {
 }
 
 type IPv6 struct {
-	IPAddress []IPAddress `json:"ipAddress"`
+	IPAddress []interface{} `json:"ipAddress"`
 }
 
-type Subnet struct {
-	Address string `json:"address"`
-	Prefix  string `json:"prefix"`
+type Interface struct {
+	IPv4       IPv4   `json:"ipv4"`
+	IPv6       IPv6   `json:"ipv6"`
+	MacAddress string `json:"macAddress"`
 }
 
-type IPAddress struct {
-	PrivateIPAddress string `json:"privateIpAddress"`
-	PublicIPAddress  string `json:"publicIpAddress"`
+type Network struct {
+	Interface []Interface `json:"interface"`
 }
